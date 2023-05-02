eb2ba30625222e6f4592b98942aaafc035433c5f
guilhermekuehl <guikuehlkeller120@gmail.com>
2023-05-02 20:09:35 -0300

    fist commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { Header } from './components/header'
+import './global.css'
+import styles from './App.module.css'
+import { Post } from './components/Post.jsx'
+import { Sidebar } from './components/Sidebar'
+
+const posts = [
+  {
+    id: 1,
+    author:{
+      avatarUrl: 'https://github.com/guilhermekuehl.png',
+      name: 'Guilherme Kellermann',
+      role: 'Software Developer @ Senior Sistemas'
+    },
+    content: [
+    { type: 'paragraph', content: 'Fala galeraa 👋', },
+    { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+    { type: 'link', content: 'jane.design/doctorcare',}
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
+  },
+  {
+    id: 2,
+    author:{
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @ Rocketseat'
+    },
+    content: [
+    { type: 'paragraph', content: 'Fala galeraa 👋', },
+    { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+    { type: 'link', content: 'jane.design/doctorcare',}
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
+  }
+];
+
+function App() {
+  return (
+  <div>
+    <Header />
+    
+    <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
+        {posts.map(post => {
+          return (
+            <Post 
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          )
+        })}
+      </main>
+    </div>
+
+  </div>
+  )
+  }
+
+export default App
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import { Avatar } from './Avatar';
+import styles from './Sidebar.module.css'
+import { PencilLine } from 'phosphor-react'
+
+export function Sidebar() {
+  return(
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover} 
+        src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/guilhermekuehl.png"/>
+        <strong>Guilherme Kuehl Kellermann</strong>
+        <spam>Full Stack Developer</spam>
+      </div>
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
